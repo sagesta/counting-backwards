34648e92ef9623e1a2d0b641264f00f940b7719e
seisa <adebodun93@gmail.com>
2022-03-11 12:15:47 +0100

    Add project files.

---
+++ counting_backwards.py
@@ -0,0 +1,17 @@
+fruit = input ("enter a word\n")
+char =''
+length = len(fruit) - 1
+
+
+#for x in range (length):
+ #   print (x,fruit[x])
+
+
+
+while (length >= - 0):
+   char += fruit[length]
+    
+   length-=1
+
+print (char)
+
